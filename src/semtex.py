dda58bdb5f204c3240187580e692d8a6af943ba1
nic <nicroland9@gmail.com>
2012-01-21 23:08:56 +0000

    Started GUI, temporarily disabled save to history and some minor fixes.
    GUI is just a text box to enter the equation and button to refresh.

--- src/semtex.py
+++ src/semtex.py
@@ -3,110 +3,182 @@ Created on 20 Jan 2012
 
 @author: nic
 '''
-import os, commands
+import os, commands, sys
+from PyQt4 import QtGui # TODO: Reduce scope of imports
 
-# Clean up
-def cleanUp():
-    file_list = commands.getoutput('ls temp.*')
-    if file_list != 'ls: cannot access temp.*: No such file or directory':
-        file_list = file_list.split('\n')
-        for row in file_list:
-            os.remove(row)
+# Misc
+icon_path = '' # TODO: Make icon, specify path
 
-# Misc Variables
-hist = [] # Buffer for recent history of commands
-hist_len = 5 # Number of equations to save to history
+class Semtex(QtGui.QWidget):
 
+    # Misc Variables
+    hist = [] # Buffer for recent history of commands
+    
+    def __init__(self):
+        super(Semtex, self).__init__()
+    
+        self.initUi()
+        self.checkDependancies()
+        self.loadHistory()
+        self.setLast()
+        
+    def initUi(self):
+        # --- Set Up Window ---
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('SemTeX: Equations Made using laTEX')
+        #self.setWindowIcon(QtGui.QIcon(icon_path)) TODO: Enable this later
+
+        # --- TextEdits ---
+        self.teInput = QtGui.QTextEdit()
+        self.teInput.resize(20,10) 
+
+        # --- Create Button ---
+        bRefresh = QtGui.QPushButton('Refresh')
+        bRefresh.clicked.connect(self.refresh)
+
+        # --- Sort Layout ---
+        vbox = QtGui.QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addWidget(self.teInput)
+        vbox.addWidget(bRefresh)
+        
+        hbox = QtGui.QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addLayout(vbox)
+        # TODO: Add Imageview
+        
+        self.setLayout(hbox)
+
+        self.show()
+    
+    def refresh(self):
+        """
+        Takes the equation from the TextEdit, compiles and converts to PNG
+        """
+        self.generateLatex()
+        self.compileLatex()
+        self.convertPng()
+        self.cleanUp()
+        
+        
+    def loadHistory(self):
+        """
+        Get last 5 entries in history, print to terminal
+        """
+        try:
+            # Check For History, Create History
+            hist_file = file('.hist','r')
+        
+            # Display Previous Equations in terminal
+            print 'Previous equations:'
+        
+            for row in hist_file:
+                self.hist.append(row.strip())
+                print '\t',row.strip()
+        except IOError, e:
+            print 'Error - accessing history'
+            print 'Details -', e
+        finally:
+            hist_file.close()
+
+    def setLast(self):
+        """
+        Check last entry in history, insert into teInput
+        """
+        self.teInput.setText(self.hist[-1])
+        
+    def generateLatex(self):
+        """
+        Generate LaTeX File
+        """
+        # Generate LaTeX File
+        try:
+            eq_start = file('.start','r')
+            eq_end = file('.end','r')
+            
+            eq = eq_start.read() + self.teInput.toPlainText() + eq_end.read()
+            
+            temp = file('temp.tex','w')
+            temp.write(eq)
+        except Exception, e:
+            print 'Error - creating .tex file'
+            print 'Details -', e
+        finally:
+            eq_start.close()
+            eq_end.close()
+            temp.close()
+        
+    def compileLatex(self):
+        """
+        Use LaTeX to compile .tex file
+        """
+        # TODO: Suppress output?
+        # Compile LaTeX
+        try:
+            os.system('latex temp.tex')
+        except Exception, e:
+            print 'Error - compiling .tex file'
+            print 'Details -', e
+            
+    def convertPng(self):
+        """
+        Converts dvi file created by latex to png
+        """
+        # TODO: Suppress output?
+        try:
+            os.system('dvipng -T tight -x 1200 -z 9 -bg transparent -o temp.png temp.dvi')
+        except Exception, e:
+            print 'Error - converting to png'
+            print 'Details -', e
+            
+    def checkDependancies(self):
+        """
+        Check For Resources: dvipng, latex
+        """
+        try:
+            latex_check = commands.getoutput('which latex')
+            dvipng_check = commands.getoutput('which dvipng')
+    
+            if (latex_check == '' or dvipng_check == ''):
+                print 'Missing resources, please make sure you have latex and dvipng installed'
+                quit() # TODO: change to throwing an exception
+        except Exception, e:
+            print 'Error - failed search for resources'
+            print 'Details -', e
+            quit()
+    
+    # Clean up
+    def cleanUp(self):
+        """
+        Get rid of unneccessary files
+        """
+        file_list = commands.getoutput('ls temp.*')
+        if file_list != 'ls: cannot access temp.*: No such file or directory': # TODO: Find a cleaner way
+            file_list = file_list.split('\n')
+            png = file_list.index('temp.png')
+            file_list.pop(png) # We don't want to remove the image
+            for row in file_list:
+                os.remove(row)
+
+#    # Save History
+#    try:
+#        while '\n' in hist:
+#            i = hist.index('\n')
+#            hist.pop(i)
+#        hist = [z+'\n' for z in hist[-hist_len:]] # Last five entries only, add endl to each
+#        hist_file = file('.hist','w')
+#        for row in hist:
+#            hist_file.write(row)
+#    except IOError, e:
+#        print e
+#    finally:
+#        hist_file.close()
+#        quit()
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    root = Semtex()
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    # Clean up files
-    cleanUp()
-    
-    # Check For Resources: dvipng, latex
-    try:
-        latex_check = commands.getoutput('which latex')
-        dvipng_check = commands.getoutput('which dvipng')
-        
-        if (latex_check == '' or dvipng_check == ''):
-            'Missing resources, please make sure you have latex and dvipng installed'
-            quit()
-    except Exception, e:
-        print 'Error - failed search for resources'
-        print 'Details -', e
-        quit()
-        
-    try:
-        # Check For History, Create History
-        hist_file = file('.hist','r')
-    
-        # Display Previous Equations
-        print 'Previous equations:'
-    
-        for row in hist_file:
-            hist.append(row.strip())
-            print '\t',row.strip()
-    except IOError, e:
-        print 'Error - accessing history'
-        print 'Details -', e
-    finally:
-        hist_file.close()
-    
-    # Ask For Equation
-    try:
-        inp = raw_input('Please enter an equation:')
-        hist.append(inp)
-    except IOError, e:
-        print 'Error - asking for equation'
-        print 'Details -', e
-        quit()
-        
-    # Generate LaTeX File
-    try:
-        eq_start = file('.start','r')
-        eq_end = file('.end','r')
-        
-        eq = eq_start.read() + hist[-1] + eq_end.read()
-        
-        temp = file('temp.tex','w')
-        temp.write(eq)
-    except Exception, e:
-        print 'Error - creating .tex file'
-        print 'Details -', e
-        cleanUp()
-        quit()
-    finally:
-        eq_start.close()
-        eq_end.close()
-        temp.close()
-        
-    # Compile LaTeX
-    try:
-        os.system('latex temp.tex')
-    except Exception, e:
-        print 'Error - compiling .tex file'
-        print 'Details -', e
-        cleanUp()
-        quit()
-    
-    # PNG Conversion
-    try:
-        os.system('dvipng -T tight -x 1200 -z 9 -bg transparent -o temp.png temp.dvi')
-    except Exception, e:
-        print 'Error - converting to png'
-        print 'Details -', e
-        cleanUp()
-        
-    # Save History
-    try:
-        while '\n' in hist:
-            i = hist.index('\n')
-            hist.pop(i)
-        hist = [z+'\n' for z in hist[-hist_len:]] # Last five entries only, add endl to each
-        hist_file = file('.hist','w')
-        for row in hist:
-            hist_file.write(row)
-    except IOError, e:
-        print e
-    finally:
-        hist_file.close()
-        quit()
+    main()
