9abe9bb82cf051a0d80cea18b6c2a4fe0872401c
nic <nicroland9@gmail.com>
2012-04-14 13:02:36 +0100

    Updated setup.py, added extra package to LaTeX template

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-#from cx_Freeze import setup, Executable #@UnresolvedImport
 from distutils.core import setup
 
 setup(
 	name = "semtex",
-	version = "0.1a",
+	version = "0.5a",
 	author='Nic Roland',
 	author_email='nicroland9@gmail.com',
 	description = "SemTeX: Equations Made using laTEX",
 	scripts = ["semtex.py"],
-	packages = ['semtex_gui'],
-	package_data = {'semtex_gui':['cache/*','res/*']})
+	packages = ['semtex'],
+	package_data = {'semtex':['cache/*','res/*']})
